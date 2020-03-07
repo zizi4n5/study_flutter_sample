9c59c4561716e056b952e77dc013f8ec85ced0e2
zizi4n5 <zizi4n5@gmail.com>
2020-03-07 21:14:44 +0900

    feat: completed lecture 56 (dicee)

---
+++ flutter_dicee_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
+  }
+}
