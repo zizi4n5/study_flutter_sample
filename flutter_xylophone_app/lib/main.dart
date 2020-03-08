f2e8e6baaae560a6d2930ded02e5a886194a0610
zizi4n5 <zizi4n5@gmail.com>
2020-03-08 13:21:31 +0900

    feat: completed lecture 81 (xylophone)

--- flutter_xylophone_app/lib/main.dart
+++ flutter_xylophone_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -11,7 +12,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: Text(nouns.last)),
         ),
       ),
     );
