5495390111c0f5814f880efb62c85da9253a0ea8
zizi4n5 <zizi4n5@gmail.com>
2020-03-08 22:40:03 +0900

    feat: completed lecture 101 (quizzler)

--- flutter_quizzler_app/lib/main.dart
+++ flutter_quizzler_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,16 +28,12 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   @override
@@ -51,7 +48,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[scoreKeeper.length % questions.length],
+                questionBank[scoreKeeper.length % questionBank.length]
+                    .questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +76,8 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
 
                 bool correctAnswer =
-                    answers[scoreKeeper.length % questions.length];
+                    questionBank[scoreKeeper.length % questionBank.length]
+                        .questionAnswer;
 
                 if (correctAnswer == true) {
                   setState(() {
@@ -118,7 +117,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 bool correctAnswer =
-                    answers[scoreKeeper.length % questions.length];
+                    questionBank[scoreKeeper.length % questionBank.length]
+                        .questionAnswer;
 
                 if (correctAnswer == false) {
                   setState(() {
