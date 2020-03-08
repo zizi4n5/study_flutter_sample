e2b2425f4be77076e4ec7c1f022a908ad2486748
zizi4n5 <zizi4n5@gmail.com>
2020-03-08 14:05:45 +0900

    feat: completed lecture 83 (xylophone)

--- flutter_xylophone_app/lib/main.dart
+++ flutter_xylophone_app/lib/main.dart
@@ -6,20 +6,62 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final player = AudioCache();
+
+  void playSound(int soundNumber) {
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
